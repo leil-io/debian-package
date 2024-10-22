36c7a7ec268acf71cf9b69ef3556cb96327b3498
Urmas Rist <urmas@urist.ee>
2024-10-22 14:44:44 +0300

    feat: Improve prepare-dev-environment.sh

--- prepare-dev-environment.sh
+++ prepare-dev-environment.sh
@@ -8,13 +8,12 @@ SOURCE_DIR="${BUILD_DIRECTORY}/saunafs-${VERSION}"
 mkdir $BUILD_DIRECTORY
 cd $BUILD_DIRECTORY
 wget https://github.com/leil-io/saunafs/archive/refs/tags/v${VERSION}.tar.gz
-mv "v${VERSION}.tar.gz" $SOURCE_TAR
+mv "v${VERSION}.tar.gz" ${SOURCE_TAR}
 
-tar xf $SOURCE_TAR
+tar xf ${SOURCE_TAR}
 rm "$SOURCE_DIR/debian" -r
-ln -s $DEBIAN_DIR $SOURCE_DIR
-
-export QUILT_PATCHES=debian/patches
-export QUILT_REFRESH_ARGS="-p ab --no-timestamps --no-index"
+ln -s ${DEBIAN_DIR} ${SOURCE_DIR}
 
 cd $SOURCE_DIR
+QUILT_PATCHES=debian/patches QUILT_REFRESH_ARGS="-p ab --no-timestamps --no-index" bash
+rm -r $BUILD_DIRECTORY
