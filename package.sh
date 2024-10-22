a30a203b73433d564eb36dd587bd9d47a4fb7707
Urmas Rist <urmas@urist.ee>
2024-10-22 12:41:24 +0300

    feat: Add building from specific ref to package.sh

--- package.sh
+++ package.sh
@@ -1,20 +1,32 @@
+set -e
 VERSION="4.5.1"
 OUTPUT_DIR="$(pwd)/build"
 BUILD_DIRECTORY="/tmp/package-saunafs"
-
-SOURCE_TAR="saunafs_${VERSION}.orig.tar.gz"
 SOURCE_DIR="${BUILD_DIRECTORY}/saunafs-${VERSION}"
+SOURCE_TAR="saunafs_${VERSION}.orig.tar.gz"
 
 mkdir -p "${OUTPUT_DIR}"
 mkdir "${BUILD_DIRECTORY}"
 cp -r debian "${BUILD_DIRECTORY}"
 cd "${BUILD_DIRECTORY}"
-wget https://github.com/leil-io/saunafs/archive/refs/tags/v${VERSION}.tar.gz
-mv "v${VERSION}.tar.gz" "${SOURCE_TAR}"
-cp "${SOURCE_TAR}" "${OUTPUT_DIR}"
 
+if [ -n "$1" ]; then
+	git clone https://github.com/leil-io/saunafs/
+	cd saunafs
+	git checkout "$1"
+	cd ..
+	mv saunafs saunafs-${VERSION}
+	tar --exclude-vcs -czf "${SOURCE_TAR}" saunafs-${VERSION}
+	rm -rf saunafs
+else
+	wget https://github.com/leil-io/saunafs/archive/refs/tags/v${VERSION}.tar.gz
+	mv "v${VERSION}.tar.gz" "${SOURCE_TAR}"
+fi
+
+cp "${SOURCE_TAR}" "${OUTPUT_DIR}"
 tar xf "${SOURCE_TAR}"
-rm "${SOURCE_DIR}/debian" -r
+
+rm "${SOURCE_DIR}/debian" -rf
 cp -r "${BUILD_DIRECTORY}/debian" "${SOURCE_DIR}"
 
 cd "$SOURCE_DIR"
