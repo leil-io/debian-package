2aa39231b282fb4905704cdbe83fc5073b154a03
Urmas Rist <urmas@urist.ee>
2024-10-23 11:56:29 +0300

    feat: Add patcing feature for package.sh

--- package.sh
+++ package.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-set -e
+set -ex
 
 VERSION="4.5.1"
 OUTPUT_DIR="$(pwd)/build"
 BUILD_DIRECTORY="/tmp/package-saunafs"
+PATCHES_DIRECTORY="${BUILD_DIRECTORY}/patches"
 
 print_help() {
 	echo "
@@ -11,16 +12,36 @@ This helper script allows quickly building saunafs debian
 packages. By default, it will download from the releases section of
 SaunaFS github, but you can also specify a option to build from a
 specific git reference, note that if you use a specific version ref, the
-packages will be confusingly named to the git version of the debian folder. Use
+packages will be confusingly named to the version of the debian folder. Use
 the tags on this repository to change to the version you want!"
 	echo "Options are:"
 	echo "-r <string>: Clone and build from a git reference"
+	echo "-p <path/to/file>: Apply a single patch file"
+	echo "-f <path/to/directory>: Apply patches from directory"
 }
 
-while getopts ":hr:" arg; do
+PATCHES=()
+
+while getopts ":hr:p:f:" arg; do
 	case $arg in
 		r)
 			REF="${OPTARG}"
+			;;
+		p)
+			if [ ! -f "${OPTARG}" ]; then
+				echo "${OPTARG} not found!"
+				exit 1
+			fi
+			PATCHES+=("${OPTARG}")
+			;;
+		f)
+			if [ ! -d "${OPTARG}" ]; then
+				echo "${OPTARG} not found!"
+				exit 1
+			fi
+			for patch in "${OPTARG}"/*.patch; do
+				PATCHES+=("${patch}")
+			done
 			;;
 		h | *) # Display help.
 			print_help
@@ -34,6 +55,12 @@ SOURCE_TAR="saunafs_${VERSION}.orig.tar.gz"
 
 mkdir -p "${OUTPUT_DIR}"
 mkdir "${BUILD_DIRECTORY}"
+mkdir "${PATCHES_DIRECTORY}"
+if [ ${#PATCHES[@]} != 0 ]; then
+	for patch in $PATCHES; do
+		cp "${patch}" "${PATCHES_DIRECTORY}"
+	done
+fi
 cp -r debian "${BUILD_DIRECTORY}"
 cd "${BUILD_DIRECTORY}"
 
@@ -60,6 +87,14 @@ cd "$SOURCE_DIR"
 mk-build-deps
 sudo apt install "./saunafs-build-deps_${VERSION}_all.deb"
 rm "./saunafs-build-deps_${VERSION}"*
+
+if [ -n "$(ls -A "${PATCHES_DIRECTORY}")" ]; then
+	for patch in "${PATCHES_DIRECTORY}"/*; do
+		QUILT_REFRESH_ARGS="-p ab --no-timestamps --no-index" QUILT_PATCHES=debian/patches \
+			quilt import "${patch}"
+	done
+fi
+
 debuild -us -uc
 # Package metadata
 cp "${BUILD_DIRECTORY}/saunafs_"* "${OUTPUT_DIR}"
