cd36bbcd35b2c86f9c9c58f51c5188bb41d90156
Urmas Rist <urmas@urist.ee>
2025-02-14 13:26:01 +0200

    refactor: Remove unneeded wget

--- install-deps.sh
+++ install-deps.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
 set -x
-. ./version.sh
-BUILD_DIRECTORY="/tmp/package-saunafs"
-export DEBIAN_FRONTEND=noninteractive
-SOURCE_DIR="${BUILD_DIRECTORY}/saunafs-${VERSION}"
-SOURCE_TAR="saunafs_${VERSION}.orig.tar.gz"
-
-apt-get install --yes equivs curl zip unzip tar
-
-rm -rf "${BUILD_DIRECTORY:?}"
-mkdir "${BUILD_DIRECTORY}"
-cp -r debian "${BUILD_DIRECTORY}"
-cd "${BUILD_DIRECTORY}" || exit 1
-wget https://github.com/leil-io/saunafs/archive/refs/tags/v${VERSION}.tar.gz
-
-mv "v${VERSION}.tar.gz" "${SOURCE_TAR}"
-tar xf "${SOURCE_TAR}"
-
-rm "${SOURCE_DIR}/debian" -rf
-cp -r "${BUILD_DIRECTORY}/debian" "${SOURCE_DIR}"
-
-cd "$SOURCE_DIR" || exit 1
+apt-get install --yes equivs devscripts
 mk-build-deps
 apt-get install --yes "./saunafs-build-deps_${VERSION}-${REVISION}_all.deb"
 rm "./saunafs-build-deps_${VERSION}-${REVISION}"*
-
-rm -rf "${BUILD_DIRECTORY:?}"
