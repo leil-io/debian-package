925d6462cb2959b04a5460f1de2e25c4689ffaf3
Urmas Rist <urmas@urist.ee>
2025-02-19 07:40:33 +0200

    fix: Add missing vcpkg dependencies

--- install-deps.sh
+++ install-deps.sh
@@ -2,9 +2,10 @@
 
 . ./version.sh
 
+export DEBIAN_FRONTEND=noninteractive
 set -x
 apt-get update
-apt-get install --yes equivs devscripts
+apt-get install --yes equivs devscripts curl zip unzip tar bison
 mk-build-deps
 apt-get install --yes "./saunafs-build-deps_${VERSION}-${REVISION}_all.deb"
 rm "./saunafs-build-deps_${VERSION}-${REVISION}"*
