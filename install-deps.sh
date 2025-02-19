fc044a0737008798e4b712818cfbd844ea3db3f0
Urmas Rist <urmas@urist.ee>
2025-02-19 07:32:00 +0200

    fix: install-deps.sh missing version

--- install-deps.sh
+++ install-deps.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+. ./version.sh
+
 set -x
+apt-get update
 apt-get install --yes equivs devscripts
 mk-build-deps
 apt-get install --yes "./saunafs-build-deps_${VERSION}-${REVISION}_all.deb"
