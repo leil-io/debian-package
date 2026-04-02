2509d12bff328db756c810f1c1e9155f91845d18
Rolando Sánchez Ramos <rolysr@leil.io>
2026-04-02 13:37:00 +0200

    chore: minimal rebrand SaunaFS to LeilFS
    This commit includes changes for minimal rebranding of documentation,
    links and references to LeilFS.
    
    Signed-off-by: Rolando Sánchez Ramos <rolysr@leil.io>

--- prepare-dev-environment.sh
+++ prepare-dev-environment.sh
@@ -3,11 +3,11 @@ BUILD_DIRECTORY="/tmp/dev-saunafs"
 DEBIAN_DIR="$(pwd)/debian"
 
 SOURCE_TAR="saunafs_${VERSION}.orig.tar.gz"
-SOURCE_DIR="${BUILD_DIRECTORY}/saunafs-${VERSION}"
+SOURCE_DIR="${BUILD_DIRECTORY}/leilfs-${VERSION}"
 
 mkdir $BUILD_DIRECTORY
 cd $BUILD_DIRECTORY
-wget https://github.com/leil-io/saunafs/archive/refs/tags/v${VERSION}.tar.gz
+wget https://github.com/leil-io/leilfs/archive/refs/tags/v${VERSION}.tar.gz
 mv "v${VERSION}.tar.gz" ${SOURCE_TAR}
 
 tar xf ${SOURCE_TAR}
--- update-changelog.sh
+++ update-changelog.sh
@@ -1,4 +1,4 @@
-export NAME="Saunafs Team"
-export EMAIL="support@saunafs.com"
-dch -v "${1}" "Update SaunaFS version"
+export NAME="LeilFS Team"
+export EMAIL="contact@leil.io"
+dch -v "${1}" "Update LeilFS version"
 dch -v "${1}" --distribution stable ""
